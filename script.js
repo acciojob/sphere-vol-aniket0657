b1f4127d6d4a51c6cc8b1134b43511c24259cb7a
Aniket Yadav <117531261+aniket0657@users.noreply.github.com>
2025-10-02 21:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
     //Write your code here
-  
-} 
+	event.preventDefault();
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+	radius = parseFloat(radius);
+
+	if(isNaN(radius)||radius < 0){
+		document.getElementById("volume").value = NaN;
+		return;
+		
+	}
+	let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	volume = volume.tofixed(4);
+	document.getElementById("volume").value = volume;
+}
+
+// Attach event listener to form
+document.getElementById("MyForm").addEventListener("submit", volume_sphere);
